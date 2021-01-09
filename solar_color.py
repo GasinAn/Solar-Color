1b19092310201b679cb28f2338477f9cc0298673
GasinAn <Gasin185@163.com>
2021-01-09 22:33:51 +0800

    add down star data code

--- solar_color.py
+++ solar_color.py
@@ -1,4 +1,20 @@
+from re import search
+from requests import get
 from sklearn.manifold import TSNE
+
+def get_star_data():
+    with open("url.txt", "r") as f:
+        urls = f.read.split("\n")
+    for url in urls:
+        try:
+            r = get(url)
+            r.raise_for_status()
+            content_disposition = r.headers["Content-disposition"]
+            filename = search("filename=(.*)", content_disposition).group(1)
+            with open(f"star_data\\{filename}", 'w') as f:
+                f.write(r.text)
+        except:
+            print(f"{url} X!")
 
 def t_SNE(data, perplexity=30.0):
     """
@@ -7,3 +23,5 @@ def t_SNE(data, perplexity=30.0):
     Return 2D points after t-SNE
     """
     return TSNE(n_components=2, perplexity=perplexity).fit_transform(data)
+
+get_star_data()
