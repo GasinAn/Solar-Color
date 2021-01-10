75e0efc573808ce19ce2c4bb725afd85d88e9a0e
GasinAn <Gasin185@163.com>
2021-01-11 05:36:44 +0800

    add decompress_star_data

--- solar_color.py
+++ solar_color.py
@@ -1,3 +1,5 @@
+from gzip import GzipFile
+from os import listdir
 from re import search
 from requests import get
 from sklearn.manifold import TSNE
@@ -6,6 +8,7 @@ def get_star_data():
     """
     Download star data from LAMOST(DR6_v2, STAR:G2)
     Need URLs of data, in url.txt
+    Print URL if fail, then download next one 
     """
     with open("url.txt", "r") as f:
         urls = f.readlines()
@@ -22,6 +25,23 @@ def get_star_data():
         except:
             print(f"{url[:-1]} X!")
 
+def decompress_star_data():
+    """
+    Decompress star data
+    Print gzip filename if fail, then decompress next one
+    """
+    fits_dir, gzip_dir = "star_data\\", "star_data\\origin\\"
+    gzip_filename_list = listdir("star_data\\origin")
+    for gzip_filename in gzip_filename_list:
+        try:
+            fits_filename = gzip_filename.replace(".gz", "")
+            with open(f"{fits_dir}{fits_filename}", "wb") as f_fits:
+                with GzipFile(f"{gzip_dir}{gzip_filename}", "rb") as f_gzip:
+                    fits_content = f_gzip.read()
+                f_fits.write(fits_content)
+        except:
+            print(f"{gzip_filename} X!")
+
 def t_SNE(data, perplexity=30.0):
     """
     data: [spec_0, spec_1,...], has shape (N_data, N_lambda)
@@ -31,3 +51,4 @@ def t_SNE(data, perplexity=30.0):
     return TSNE(n_components=2, perplexity=perplexity).fit_transform(data)
 
 #get_star_data()
+#decompress_star_data()
