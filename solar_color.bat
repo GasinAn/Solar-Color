82c0ac68da912dc180f1ee241ce5b86e604e2049
GasinAn <Gasin185@163.com>
2021-01-14 02:13:59 +0800

    add OK bat

--- solar_color.bat
+++ solar_color.bat
@@ -1,4 +1,11 @@
-cd .\solar_data
+mkdir .\star_catalog
+mkdir .\star_data
+mkdir .\star_data\original
+mkdir .\star_data\decompressed
+mkdir .\star_data\normalized
+mkdir .\sun_data
+mkdir .\sun_data\normalized
+cd .\sun_data
 python .\convolute_solar_spec.py
 copy .\lumda_spec.npy .\sun_spec.npy
 cd ..
