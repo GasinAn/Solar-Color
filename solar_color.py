0d58165f466af30c1adf09ab1122e97625027f9c
GasinAn <Gasin185@163.com>
2021-01-13 16:41:30 +0800

    modify get_selected_star_data; del tSNE

--- solar_color.py
+++ solar_color.py
@@ -9,7 +9,6 @@ from gzip import GzipFile
 from os import listdir
 from re import search
 from requests import get
-from sklearn.manifold import TSNE
 
 def get_star_data():
     """
@@ -25,7 +24,7 @@ def get_star_data():
             r.raise_for_status()
             content_disposition = r.headers["Content-disposition"]
             filename = search("filename=(.*)", content_disposition).group(1)
-            with open(f"star_data\\origin\\{filename}", "wb") as f:
+            with open(f"star_data\\original\\{filename}", "wb") as f:
                 for chunk in r.iter_content(chunk_size=1024):
                     if chunk:
                         f.write(chunk)
@@ -36,9 +35,10 @@ def decompress_star_data():
     """
     Decompress star data
     Print gzip filename if fail, then decompress next one
+    Actually only used for test whether OK to read gzip file 
     """
-    fits_dir, gzip_dir = "star_data\\", "star_data\\origin\\"
-    gzip_filename_list = listdir("star_data\\origin")
+    fits_dir, gzip_dir = "star_data\\decompressed", "star_data\\origin\\"
+    gzip_filename_list = listdir("star_data\\original")
     for gzip_filename in gzip_filename_list:
         try:
             fits_filename = gzip_filename.replace(".gz", "")
@@ -49,35 +49,33 @@ def decompress_star_data():
         except:
             print(f"{gzip_filename} X!")
 
-def get_sun_like_star_data():
+def get_selected_star_data():
     """
     Use T_eff, log(g) and [Fe/H] in catalog to get sun-like star data, with:
     |T_eff-5770|<200 & |log(g)-4.43775|<0.2 & |[Fe/H]|<0.5
-    Return (sun-like star file names, sun-like star spec obsids)
     """
-    star_filenames = listdir("star_data")[1:]
-    with fits.open("star_catalog\\dr6_v2_stellar_LRS.fits") as hdul:
+    star_params = ("tff", "tff_err", "logg", "logg_err", "feh", "feh_err")
+    with fits.open("star_catalog\\dr6_v2_stellar_LRS.fits.gz") as hdul:
         catalog = hdul[1].data
     catalog = catalog[catalog["subclass"]=="G2"]
     catalog = catalog[abs(catalog["logg"]-4.43775)<0.2]
     catalog = catalog[abs(catalog["feh"])<0.5]
     catalog = catalog[abs(catalog["teff"]-5770)<200]
-    selected_spec_obsids = catalog["obsid"]
-    selected_star_filenames = []
-    for filename in star_filenames:
-        with fits.open(f"star_data\\{filename}") as hdul:
-            if hdul[0].header["OBSID"] in selected_spec_obsids:
-                selected_star_filenames.append(filename)
-    return selected_star_filenames, selected_spec_obsids
-
-def t_SNE(data, perplexity=30.0):
-    """
-    data: [spec_0, spec_1,...], has shape (N_data, N_lambda)
-    perplexity: perplexity
-    Return 2D points after t-SNE
-    """
-    return TSNE(n_components=2, perplexity=perplexity).fit_transform(data)
+    obsid = catalog["obsid"]
+    obsid2index = {}
+    for index in range(len(catalog["obsid"])):
+        obsid2index[obsid[index]] = index
+    for star_param in star_params:
+        exec(f"{star_param}=catalog['{star_param}']")
+    star_data_filenames = listdir("star_data\\original")
+    for filename in star_data_filenames:
+        with fits.open(f"star_data\\original\\{filename}") as hdul:
+            if hdul[0].header["OBSID"] in obsid:
+                index = obsid2index[hdul[0].header["OBSID"]]
+                for star_param in star_params:
+                    exec(f"hdul[0].header['{star_param}']={star_param}[index]")
+                hdul.writeto(f"star_data\\selected\\{filename}")
 
 # get_star_data()
 # decompress_star_data()
-# star_filenames, spec_obsids = get_sun_like_star_data()
+# get_selected_star_data()
